a72722bb9341ed349edfe5969f4fe4038ff62ea6
Dmytro Soltys <soap@slotos.net>
2023-05-19 14:25:51 +0200

    Add E2E test gluing everything together

--- spec/currency/api/adapters/httpx_spec.rb
+++ spec/currency/api/adapters/httpx_spec.rb
@@ -1,34 +1,17 @@
 # frozen_string_literal: true
 
 require "currency/api/adapters/httpx"
-require "webrick"
 
 RSpec.describe Currency::Api::Adapters::HTTPX do
-  around(:all) do |all|
-    @started = false
-    @server = ::WEBrick::HTTPServer.new(
-      BindAddress: "127.0.0.1",
-      Port: 3001,
-      StartCallback: -> { @started = true }
-    )
+  let(:path) { RSpec.configuration.web_server.mount_echo("Hello!") }
 
-    @server.mount_proc("/") do |request, response|
-      response.body = "Hello!"
-    end
-
-    Thread.new { @server.start }
-    Timeout.timeout(1) { :wait until @started }
-
-    all.run
-  ensure
-    @server.shutdown
-  end
+  after { RSpec.configuration.web_server.umount(path) }
 
   describe "#get" do
     subject { described_class.new }
 
     it "returns response body as a string" do
-      expect(subject.get("http://127.0.0.1:3001/")).to eq("Hello!")
+      expect(subject.get("http://127.0.0.1:3001#{path}")).to eq("Hello!")
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,40 @@
 # frozen_string_literal: true
 
 require "currency/converter"
+require "webrick"
+require "securerandom"
+
+class TestServer
+  include Singleton
+
+  def initialize
+    @server = ::WEBrick::HTTPServer.new(
+      BindAddress: "127.0.0.1",
+      Port: 3001,
+      StartCallback: -> { @started = true }
+    )
+
+    Thread.new { @server.start }
+    Timeout.timeout(1) { :wait until @started }
+  end
+
+  def mount_echo(echo, suffix: "")
+    Pathname("/#{SecureRandom.uuid}")
+      .tap do |path|
+        @server.mount_proc((path + suffix.to_s).to_s) do |request, response|
+          response.body = echo
+        end
+      end
+  end
+
+  def umount(path, suffix: "")
+    @server.umount((Pathname(path) + suffix.to_s).to_s)
+  end
+
+  def shutdown
+    @server.shutdown
+  end
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -12,4 +46,8 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.add_setting :web_server
+  config.web_server = TestServer.instance
+  config.after(:suite) { config.web_server.shutdown }
 end
